2d214bf12c0cce873be63055b781b7d38335aaf4
Nate Baer <njbbaer@gmail.com>
2021-03-14 19:10:39 -0700

    Use ESLint

--- learnyounode/baby-steps.js
+++ learnyounode/baby-steps.js
@@ -1,6 +1,6 @@
-args = process.argv.slice(2)
-sum = 0
-for (var i = 0; i < args.length; i++) {
-  sum += parseInt(args[i])
+const args = process.argv.slice(2);
+let sum = 0;
+for (let i = 0; i < args.length; i += 1) {
+  sum += parseInt(args[i]);
 }
-console.log(sum)
+console.log(sum);
--- learnyounode/my-first-io.js
+++ learnyounode/my-first-io.js
@@ -1,6 +1,6 @@
-const fs = require('fs')
+const fs = require('fs');
 
-filepath = process.argv[2]
-let file = fs.readFileSync(filepath).toString()
-let numLines = file.split('\n').length - 1
-console.log(numLines)
+const [, , filepath] = process.argv;
+const file = fs.readFileSync(filepath).toString();
+const numLines = file.split('\n').length - 1;
+console.log(numLines);
